c837ea972f18c062cd6184c5340a96e07c5196aa
Edward Chiu <edwardchiu@EdwarddeMacBook-Pro.local>
2024-11-19 19:19:01 +0800

    compute type and intro similarity scores

--- modules/recommend.py
+++ modules/recommend.py
@@ -1,5 +1,7 @@
 from google.oauth2.service_account import Credentials
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.metrics.pairwise import cosine_similarity
+from transformers import BertTokenizer, BertModel
 import pandas as pd
 import numpy as np
 import gspread
@@ -16,7 +18,7 @@ class AnimeRecommend:
         self.spreadsheet = self.client.open(settings.sheetname)
         self.worksheet = self.spreadsheet.worksheet(settings.tabnames[3])
         self.df_anime = self.read_anime_data()
-        self.similarity_df = self.compute_similarity_score()
+        # self.similarity_df = self.compute_similarity_score()
 
     def read_anime_data(self):
         df_anime = pd.read_csv('data/all_anime.csv')
@@ -57,7 +59,7 @@ class AnimeRecommend:
         union = len(set1.union(set2))
         return intersection / union
 
-    def compute_similarity_score(self):
+    def compute_type_similarity_score(self):
         df_anime = self.df_anime.copy()
 
         # Create an empty list to store the results
@@ -75,11 +77,59 @@ class AnimeRecommend:
                     similarity_matrix[c, r] = similarity
 
         similarity_df = pd.DataFrame(similarity_matrix, index=df_anime['name'], columns=df_anime['name'])
-        print('Finish computing similarity score!!!')
-        return similarity_df.copy()
+        print('Finish computing anime type similarity score!!!')
+        similarity_df.to_csv('data/anime_type_similarity.csv')
+        # return similarity_df.copy()
+
+    def compute_intro_similarity_score(self):
+        # Load the tokenizer and model
+        tokenizer = BertTokenizer.from_pretrained("hfl/chinese-roberta-wwm-ext")
+        model = BertModel.from_pretrained("hfl/chinese-roberta-wwm-ext")
+
+        def intro_feature_extraction(intro):
+            # helper function to extract anime intro to feature vector
+            try:
+                # Tokenize and encode the input text
+                inputs = tokenizer(intro, return_tensors="pt", truncation=True, max_length=512)
+                outputs = model(**inputs)
+
+                # Get the hidden states
+                hidden_states = outputs.last_hidden_state
+
+                # Take the embedding of the [CLS] token (first token)
+                cls_embedding = hidden_states[:, 0, :]  # Shape: (batch_size, hidden_dim)
+                feature = cls_embedding.squeeze().detach().tolist()
+                return feature
+            except:
+                return None
+
+        df_anime = self.df_anime.copy()
+        df_anime['intro'] = df_anime['intro'].fillna(df_anime['name'])
+        df_anime['intro_feature'] = df_anime['intro'].apply(intro_feature_extraction)
+
+        df_intro = df_anime[['name', 'intro_feature']].copy()
+
+        features = np.vstack(df_intro['intro_feature'])
+
+        # Calculate cosine similarity
+        cosine_sim = cosine_similarity(features)
+        cosine_sim = (cosine_sim - cosine_sim.min()) / (cosine_sim.max() - cosine_sim.min())
+
+        np.fill_diagonal(cosine_sim, -np.inf)
+
+        anime_names = df_intro['name']
+        cosine_sim_df = pd.DataFrame(cosine_sim, index=anime_names, columns=anime_names)
+
+        print('Finish computing anime intro similarity score!!')
+        cosine_sim_df.to_csv('data/anime_intro_similarity.csv')
 
     def anime_recommend(self):
-        similarity_df = self.similarity_df.copy()
+        anime_type_similarity = pd.read_csv('data/anime_type_similarity.csv')
+        anime_type_similarity = anime_type_similarity.set_index('name')
+
+        anime_intro_similarity = pd.read_csv('data/anime_intro_similarity.csv')
+        anime_intro_similarity = anime_intro_similarity.set_index('name')
+
         df_anime = self.df_anime.copy()
 
         target_anime = self.worksheet.get_values('B1:D1')[0]
@@ -89,7 +139,7 @@ class AnimeRecommend:
         print('Obtained the target animes and metrics.')
 
         # Get the row corresponding to the target anime
-        target_similarities = similarity_df.loc[:, target_anime]
+        target_similarities = anime_type_similarity.loc[:, target_anime]
 
         scaled_metrics = df_anime.loc[:, ['name', 'scaled_launch', 'scaled_view', 'scaled_score', 'link']]
         scaled_metrics = scaled_metrics.set_index('name')
@@ -104,3 +154,9 @@ class AnimeRecommend:
         print('Determine recommended anime!!!')
 
         return top_recommend_anime.apply(lambda row: f'=HYPERLINK("{row['link']}", "{row['name']}")', axis=1)
+
+
+if __name__ == '__main__':
+    ar = AnimeRecommend()
+    ar.compute_type_similarity_score()
+    ar.compute_intro_similarity_score()
